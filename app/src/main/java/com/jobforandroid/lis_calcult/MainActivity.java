4abbc3bf2c9c7f43937ec5ef2db02c906b4cf5c2
Galyminsky-PC <jobforandroid@protonmail.com>
2021-10-30 14:41:25 +0600

    Виорая Версия - ошибка и вылет с прилоения пр наатии спец кнопок

--- app/src/main/java/com/jobforandroid/lis_calcult/MainActivity.java
+++ app/src/main/java/com/jobforandroid/lis_calcult/MainActivity.java
@@ -13,116 +13,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    final int MENU_RESET_ID = 1;
-    final int MENU_QUIT_ID = 2;
-    
-    EditText etNum1, etNum2;
-    Button btnAdd, btnSub, btnMult, btnDiv;
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,btn0;
-    Button btnPoint, btnExactly;
-    TextView tvResult;
-    
-    String oper = " ";
+    TextView txt;
+    int buffer;
+    char op;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        etNum1 = (EditText) findViewById(R.id.etNum1);
-        etNum2 = (EditText) findViewById(R.id.etNum2);
-        
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnSub = (Button) findViewById(R.id.btnSub);
-        btnMult = (Button) findViewById(R.id.btnMult);
-        btnDiv = (Button) findViewById(R.id.btnDiv);
-        btnPoint = (Button) findViewById(R.id.btnPoint);
-        btnExactly = (Button) findViewById(R.id.btnExactly);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
-        btn9 = (Button) findViewById(R.id.btn9);
-        btn0 = (Button) findViewById(R.id.btn0);
+        Button ans = findViewById(R.id. equal);
+        ans.setOnClickListener(view -> {
+            switch (op) {
+                case '+':
+                    buffer += Integer.parseInt( txt.getText().toString());
+                    txt.setText(String.valueOf(buffer));
+                    break;
+                case '-':
+                    buffer -= Integer.parseInt( txt.getText().toString());
+                    txt.setText(String.valueOf(buffer));
+                    break;
+                case '*':
+                    buffer *= Integer.parseInt( txt.getText().toString());
+                    txt.setText(String.valueOf(buffer));
+                    break;
+                case '/':
+                    buffer/= Integer.parseInt( txt.getText().toString());
+                    txt.setText(String.valueOf(buffer));
+                    break;
+            }
+        });
 
-        tvResult = (TextView) findViewById(R.id.tvResult);
+        txt = (TextView) findViewById(R.id.txt);
 
-        btnAdd.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnMult.setOnClickListener(this);
-        btnDiv.setOnClickListener(this);
+    }
+    public void ops (View view) {
+        buffer = Integer.parseInt(txt.getText().toString());
+        Button b = (Button) view;
+        op = b.getText().charAt(0);
+        txt.setText("");
     }
 
-    @Override
-    public void onClick(View v) {
-
-        float num1 = 0;
-        float num2 = 0;
-        float result = 0;
-
-        //Проверка поля на пустоту
-        if (TextUtils.isEmpty(etNum1.getText().toString()) || TextUtils.isEmpty(etNum2.getText().toString())) {
-            return;
-        }
-
-        //Читаем EditText и заполняем переменные числами
-        num1 = Float.parseFloat(etNum1.getText().toString());
-        num2 = Float.parseFloat(etNum2.getText().toString());
-
-
-        //Определяем нажатую кнопку и выполняем соотвествующию операцию
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                oper = "+";
-                result = num1 + num2;
-                break;
-            case R.id.btnSub:
-                oper = "-";
-                result = num1 - num2;
-                break;
-            case R.id.btnMult:
-                oper = "*";
-                result = num1 * num2;
-                break;
-            case R.id.btnDiv:
-                oper = "/";
-                result = num1 / num2;
-                break;
-            default:
-                break;
-        }
-        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    public void appendText (View view) {
+        Button b = (Button) view;
+        txt.setText(txt.getText().toString() + b.getText().toString());
     }
 
-    //Создание меню
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, MENU_RESET_ID, 0, "Reset");
-        menu.add(0, MENU_QUIT_ID, 0, "Quit");
-        return super.onCreateOptionsMenu(menu);
-    }
 
-    //Обработка нажатий на пункт меню
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET_ID:             //Очищаем поля
-                etNum1.setText("");
-                etNum2.setText("");
-                tvResult.setText("");
-                break;
-            case MENU_QUIT_ID:              //Выход из приложения
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
